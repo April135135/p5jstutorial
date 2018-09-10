d4adc0c92932d2cbad591bb2a2da989f61b04ed4
April135135 <42793222+April135135@users.noreply.github.com>
2018-09-10 12:12:09 +0800

    Add files via upload

---
+++ tutorial 1/sketch.js
@@ -0,0 +1,33 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  createCanvas(700,500);
+  
+}
+
+function draw()
+{
+  //create a background using RGB values
+  background(95,55,131)
+
+  // change the paint brush to a specific color according to RGB values
+  fill(230,220,50);
+
+  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  rect(500,400,200,100)
+
+  rect(0,0,200,100)
+ 
+  rect(700/2 - 200/2 , 500/2 - 100/2,200,100)
+
+  rect(0,400,200,100)
+
+  rect(500,0,200,100)
+
+
+ 
+ 
+ 
+  
+  
+}
